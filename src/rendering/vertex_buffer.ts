d2d2a646bdf23083ebede1b19b4b8b4e76a057f4
alle0017 <allegretti0017@gmail.com>
2026-06-07 19:46:11 +0200

    renderer - caching pointers across executions

--- src/rendering/vertex_buffer.ts
+++ src/rendering/vertex_buffer.ts
@@ -4,6 +4,7 @@ import { BindableBuffer } from "./bindable_buffer.js";
 import { Program } from "./program.js";
 
 export class VertexBuffer extends BindableBuffer {
+      private readonly map: WeakMap<Program,number> = new WeakMap;
       constructor(
             pass: RenderPass, 
             name: string, 
@@ -14,9 +15,16 @@ export class VertexBuffer extends BindableBuffer {
 
       public override bind(program: Program): void {
             const gl = this.pass.ctx;
-            const ptr = program.getVertexAttributeLocation(this.name);
+            let ptr: number;
 
-            gl.enableVertexAttribArray(ptr);
+            if (!this.map.has(program)) {
+                  ptr = program.getVertexAttributeLocation(this.name);
+      
+                  gl.enableVertexAttribArray(ptr);
+                  this.map.set(program, ptr);
+            } else {
+                  ptr = this.map.get(program)!;
+            }
             gl.bindBuffer(this.bufferType, this.buffer);
             gl.vertexAttribPointer(ptr, this.size, toGLType(gl, this.type), this.normalized, this.stride, this.offset)
       }
